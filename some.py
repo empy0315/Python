db1e826883890c95d2d59c6b1328bdf2b57af786
empy0315 <42884652+empy0315@users.noreply.github.com>
2018-09-08 12:32:32 +0600

    Some.py
    firstname = "Jepay"
    lastname = "Viernes"
    # We can access each character by using <your string>[<index>]
    # Each character in a string is represented by an index number
    # starting from 0
    print(firstname[0], firstname[1], firstname[2],firstname[3])
    print(lastname[0], lastname[1], lastname[2])
    
    # TRY TO PRINT THE LAST CHARACTER
    #
    # TRY TO PRINT AN INDEX GREATER THAN THE LENGTH BY UNCOMMENTING THE CODE BELOW
    # firstname[10]

---
+++ some.py
@@ -0,0 +1,12 @@
+firstname = "Jepay"
+lastname = "Viernes"
+# We can access each character by using <your string>[<index>]
+# Each character in a string is represented by an index number
+# starting from 0
+print(firstname[0], firstname[1], firstname[2],firstname[3])
+print(lastname[0], lastname[1], lastname[2])
+
+# TRY TO PRINT THE LAST CHARACTER
+#
+# TRY TO PRINT AN INDEX GREATER THAN THE LENGTH BY UNCOMMENTING THE CODE BELOW
+# firstname[10]
